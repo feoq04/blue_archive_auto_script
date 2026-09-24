d0e500672e78f146fd11e2c9ad0f88715449884a
feoq04 <firstemperorofqin0422@gmail.com>
2026-09-25 02:56:52 +0800

    Update CN activity
    - 将上次更新的`JP_2025_06_25`修改为国际服的英文活动名，因未在国际服中找到对应活动，故采用[kivo.wiki](https://kivo.wiki/timeline/4694)记录的`A Flower Blooms Among the Hundred: Fair and Square Aquatic Showdown`
    - 更新 2026-09-24 国服新活动`夏日天空的约定`适配，命名沿用国际服的英文活动名`The Promise of the Summer Sky`

--- src/images/CN/x_y_range/activity/AFlowerBloomsAmongtheHundredFairandSquareAquaticShowdown.py
+++ src/images/CN/x_y_range/activity/AFlowerBloomsAmongtheHundredFairandSquareAquaticShowdown.py
@@ -1,5 +1,5 @@
 prefix = "activity"
-path = "activity/JP_2025_06_25"
+path = "activity/AFlowerBloomsAmongtheHundredFairandSquareAquaticShowdown"
 x_y_range = {
     'enter1': (1157, 168, 1225, 222),
     'enter2': (65, 141, 178, 216),
